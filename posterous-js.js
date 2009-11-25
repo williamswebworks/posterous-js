2ee447394d9b8bfeda8a5f2a0f0ad5ddb0c1a9e6
Bryan Grohman <bgrohman@gmail.com>
2009-11-25 00:21:33 -0600

    Initial commit of posterous-js.js

---
+++ posterous-js.js
@@ -0,0 +1,74 @@
+// posterous-js.js
+// Created by Bryan Grohman
+// bgrohman@gmail.com
+
+if (!posterousjs) { var posterousjs = {}; }
+
+posterousjs.getImages = function(site_id, callback, options) {
+    var opts = options;
+    // Possible options:
+    // num_posts (integer, default 1)
+    // num_images ('all', or an integer, default 1)
+    // image_size ('medium' or 'thumb', default 'medium')
+    // tag (tag name to filter posts, default is not present)
+    var defaults = {
+        num_posts: 1,
+        num_images: 1, // number of images per post
+        image_size: 'medium' // can be 'medium' or 'thumb'
+    };
+    var attr = function(a) {
+        if (opts) {
+            return opts[a] ? opts[a] : defaults[a];
+        }
+        else {
+            return defaults[a];
+        }
+    };
+
+    var parse = function(xml) {
+        var images = [];
+        var posts = $(xml).find('rsp:first').find('post');
+        for (var j = 0; j < posts.length && j < attr('num_posts'); j++) {
+            var link = $(posts[j]).find('link:first').text();
+            var title = $(posts[j]).find('title:first').text();
+            var date_str = $(posts[j]).find('date:first').text();
+            var media_tags = $(posts[j]).find('media');
+            for (var i = 0; i < media_tags.length && (attr('num_images') == 'all' || i < attr('num_images')); i++) {
+                var media_type = $(media_tags[i]).find('type:first').text();
+                if (media_type == 'image') {
+                    var img = $(media_tags[i]).find(attr('image_size')+':first');
+                    var u = $(img).find('url:first').text();
+                    var w = $(img).find('width:first').text();
+                    var h = $(img).find('height:first').text();
+                    images.push({
+                        'id': 'post_'+j+'_image_'+i, 
+                        'url':u, 
+                        'width': w, 
+                        'height': h, 
+                        'post_link': link, 
+                        'post_title': title, 
+                        'date': date_str
+                    });
+                }
+            }
+        };
+        if (callback != undefined || callback != null) {
+            callback(images);    
+        }
+    };
+
+    var request_opts = {'site_id':site_id, 'num_posts':attr('num_posts')};
+    if (attr('tag')) {
+        request_opts['tag'] = attr('tag');
+    }
+    $.ajax({
+        type: 'GET',
+        url: 'http://posterous.com/api/readposts',
+        data: request_opts,
+        dataType: 'xml',
+        success: parse,
+        error: function() {
+            alert('posterousjs error');
+        }
+    });
+};
